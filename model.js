f4b3d77c8e5cb9d7c852db61ed7497bc1a1dd255
Siyasanga <siyasanga@live.co.za>
2017-08-28 16:43:31 +0200

    change schema from plate to plates

--- model.js
+++ model.js
@@ -4,12 +4,12 @@ mongoose.model("regdb",{
 })
 var mongoURL = process.env.MONGO_DB_URL || "mongodb://localhost/regdb";
 mongoose.connect(mongoURL,function(err,result) {
-  if(error)
+  if(err)
     console.log(err);
   else
     console.log("Database Connenction Established!");
 });
-var regNumModel = mongoose.model("plate",{
+var regNumModel = mongoose.model("plates",{
   regNum:String
 });
 module.exports = regNumModel;

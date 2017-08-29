4626bdc6c2eb945c17088a8a0c956ee9a7356264
Siyasanga <siyasanga@live.co.za>
2017-08-29 16:03:11 +0200

    Fix flash messages

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 const express = require('express');
+const flash = require('express-flash');
+const session = require('express-session');
 const exhbs = require('express-handlebars');
 const body = require("body-parser");
 const regs = require('./model');
-const flash = require('express-flash');
-const session = require('express-session');
-const cookieParser = require('cookie-parser');
 const app =  express();
-app.use(cookieParser());
 app.use(body.urlencoded({
   extended:true
 }));
@@ -31,17 +29,27 @@ app.post("/reg_numbers",function(req, res) {
   if(req.body.regNumba.length<1){
     req.flash("blank","Please enter the registration number.");
     res.redirect("/reg_numbers");
+    return;
   }
-  regs.findeOne({regNum:req.body.reg},function(err,doc) {
-    if(err){
-      var newPlate = new regs({regNum:req.body.reg});
+  regs.findOne({regNum:req.body.regNumba},function(err,doc) {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    else if(doc){
+      req.flash("duplicate",req.body.regNumba+" already exists in the database.");
+      res.redirect("/reg_numbers");
+    }
+    else {
+
+      var newPlate = new regs({regNum:req.body.regNumba});
       newPlate.save(function(err,doc) {
         if(err)
-          console.log(err);
+        console.log(err);
         else {
-            console.log(req.body.reg+" successfully saved to the database.");
-            req.flash("success",req.body.reg+" successfully saved to the database.");
-            res.redirect("/reg_numbers");
+          console.log(req.body.regNumba+" successfully saved to the database.");
+          req.flash("success",req.body.regNumba+" successfully saved to the database.");
+          res.redirect("/reg_numbers");
         }
       });
     }
@@ -49,6 +57,8 @@ app.post("/reg_numbers",function(req, res) {
 });
 app.get("/:filter",function(req, res) {
   var reg = req.params.filter;
+  if(reg == "All")
+    reg = "";
   regs.find({regNum:{$regex: reg}},function(err, db) {
     if(err)
       console.log(err);
